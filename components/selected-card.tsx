461e9245d12c186602fdfa1da8c5b67d071f511d
Artur Pedrosa <apedrosa.dev@gmail.com>
2023-09-15 16:29:42 -0300

    adjust selected card details

--- components/selected-card.tsx
+++ components/selected-card.tsx
@@ -15,24 +15,19 @@ export const SelectedCard: React.FC<AddressInsightProps | ErrorProps > = ({data,
   const {solarPotential} =  data!;
   const {message}: ErrorProps = data?.solarPotential === undefined  ? data : "";
 
-const handleSavings = () => {
-  return `R$`
-}
-
-
   return (
     <div className='text-left mt-2'>
       <p className='text-lg font-bold'>{address}</p>
       {data?.solarPotential !== undefined ? (
         <>
           <p className="text-sm font-normal">
-            Max. panels capacity: {solarPotential.maxArrayPanelsCount}
+            Max. panels capacity: {solarPotential.maxArrayPanelsCount} units
           </p>
           <p className="text-sm font-normal">
             Generation Potential: {solarPotential.maxSunshineHoursPerYear}
           </p>
           <p className="text-sm font-normal">
-            Expected Savings: {handleSavings()}
+            Solar Capacity: {solarPotential.panelCapacityWatts * solarPotential.maxArrayPanelsCount} kW
           </p>
         </>        
       ) : <p className='text-sm text-red-500'>Erro: {data?.solarPotential !== undefined ? '' : message} </p>
